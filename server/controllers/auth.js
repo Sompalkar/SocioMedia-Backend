0037f12627c87497f8eba8d605adc5a8a8c7fbed
Sompalkar <palkarsom1@gmail.com>
2023-09-07 14:59:03 +0530

    updates:

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,9 +44,9 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist. " });
-    console.log(user); // Add this line before the password comparison
+    // console.log(user); // Add this line before the password comparison
     const isMatch = await bcrypt.compare(password, user.password);
-     
+
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
